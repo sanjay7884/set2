87e5432d50a4873c76aa2c11b24c725adcd1ecb6
sanjay7884 <46869026+sanjay7884@users.noreply.github.com>
2019-01-25 17:57:41 +0000

    Create ExponentK.java

---
+++ ExponentK.java
@@ -0,0 +1,14 @@
+import jva.io.*;
+import java.util.*;
+public class ExponentK
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter value= ");
+int value=sc.nextInt();
+System.out.println("Enter Power value=");
+int power=sc.nextInt();
+System.out.println("OUTPUT="+(value^power));
+}
+}
