c9194dd0e3456237bdc2a6dc7f456d180f463776
sanjay7884 <46869026+sanjay7884@users.noreply.github.com>
2019-01-25 17:58:26 +0000

    Create OddIntervel.java

---
+++ OddIntervel.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+public class OddIntervel
+{
+public static void main(String []args)
+ {
+   int n=0,i=0;
+   Scanner X = new Scanner(System.in);
+   System.out.print(" ");
+   n = X.nextInt();
+   for(i=1; i<n; i++)
+    {
+      if(i%2!=0)
+      System.out.print(i+" ");
+     }   
+      System.out.println();
+         
+  }
+ }
